cee3e811849baa2edf39dea5f90dd677f9ec3ab5
harshitha <harshitha.ramesh2711@gmail.com>
2026-08-30 15:14:49 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/bioc-super-market-demo/'
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
